f34186b37f40ec5b0eda9d5483d501e1e0dab53a
TheAznCoderPro <155333241+TheAznCoderPro@users.noreply.github.com>
2024-02-17 22:36:07 +1100

    get input from command line

--- src/jpegxldec-cpp.cc
+++ src/jpegxldec-cpp.cc
@@ -1,5 +1,13 @@
 #include <iostream>
 
-int main() {
-    printf("Hello, world!\n");    
+int main(int argc, char* argv[]) {
+    if(argc == 0) {
+        exit(0);
+    }
+    if(argc == 1) {
+        printf("Usage: %s input.jxl", argv[0]);
+        exit(0);
+    }
+    const char* jpegxl_source_image = argv[1];
+    printf("Input file: %s\n",jpegxl_source_image); 
 }
